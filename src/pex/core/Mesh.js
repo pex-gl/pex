2a3dddb71d50131ce226426193b7334813089063
Marcin Ignac <marcin.ignac@gmail.com>
2011-10-20 20:08:34 +0200

    Whitespace fix

--- src/pex/core/Mesh.js
+++ src/pex/core/Mesh.js
@@ -19,16 +19,7 @@ define(["pex/core/Context", "pex/core/Vbo", "pex/core/Vec3", "pex/core/Vec4", "p
     }
     else {
       this.geometry = meshData;
-      if (this.geometry.vertices.length > Geometry.MAX_VERTICES) {
-        Util.log("Mesh.Mesh numVertices " + this.geometry.vertices.length + " > " + Geometry.MAX_VERTICES + ". Splitting...");
-        var geometries = this.geometry.split();
-        for(var i=0; i<geometries.length; i++) {
-          this.vbos.push(Vbo.fromGeometry(geometries[i], this.options.primitiveType, this.options.useEdges));
-        }
-      }
-      else {
-        this.vbos.push(Vbo.fromGeometry(this.geometry, this.options.primitiveType, this.options.useEdges));
-      }
+      this.buildVbosFromGeometry();
     }
 
     this.position = new Vec3(0,0,0);
@@ -36,13 +27,34 @@ define(["pex/core/Context", "pex/core/Vbo", "pex/core/Vec3", "pex/core/Vec4", "p
     this.scale = new Vec3(1, 1, 1);
   }
 
+  Mesh.prototype.buildVbosFromGeometry = function() {
+    for(var i=0; i<this.vbos.length; i++) {
+      this.vbos[i].dispose();
+    }
+    this.vbos = [];
+
+    if (this.geometry.vertices.length > Geometry.MAX_VERTICES) {
+      Util.log("Mesh.Mesh numVertices " + this.geometry.vertices.length + " > " + Geometry.MAX_VERTICES + ". Splitting...");
+      var geometries = this.geometry.split();
+      for(var i=0; i<geometries.length; i++) {
+        this.vbos.push(Vbo.fromGeometry(geometries[i], this.options.primitiveType, this.options.useEdges));
+      }
+    }
+    else {
+      this.vbos.push(Vbo.fromGeometry(this.geometry, this.options.primitiveType, this.options.useEdges));
+    }
+  }
+
   Mesh.prototype.draw = function(camera) {
+    if (this.geometry && this.geometry.dirty) {
+      this.buildVbosFromGeometry();
+    }
 
     if (camera) {
       this.material.uniforms.projectionMatrix = camera.getProjectionMatrix();
       this.material.uniforms.viewMatrix = camera.getViewMatrix();
       this.material.uniforms.modelViewMatrix = camera.calcModelViewMatrix(this.position, this.rotation, this.scale);
-  	  this.material.uniforms.normalMatrix = this.material.uniforms.modelViewMatrix.dup().invert().transpose();
+      this.material.uniforms.normalMatrix = this.material.uniforms.modelViewMatrix.dup().invert().transpose();
     }
 
     this.material.use();
