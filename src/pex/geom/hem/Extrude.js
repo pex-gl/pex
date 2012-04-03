29850fb9c186f0d97c4f78bd10682b42696d868a
Marcin Ignac <marcin.ignac@gmail.com>
2012-04-03 23:38:28 +0200

    New fast version of extrude

--- src/pex/geom/hem/Extrude.js
+++ src/pex/geom/hem/Extrude.js
@@ -11,6 +11,7 @@ define([
 
   Extrude.prototype.apply = function(hemesh, height, selection) {
     var numFaces = hemesh.faces.length;
+
     for(var f=0; f<numFaces; f++) {
       var face = hemesh.faces[f];
 
@@ -18,73 +19,31 @@ define([
 
       var normal = face.getNormal();
       var edge = face.edge;
-      var newVertices = [];
-      var newFaces = [];
+      var lastEdge = edge.findPrev();
+      var edgeToSplit = edge;
+      var prevNewEdge = null;
+      var center = face.getCenter();
       var newEdges = [];
 
+      //we split all the corners withing the face effectively adding new internal vertices
       do {
-        var newPoint = edge.vert.dup().add(normal.scaled(height));
-        var newVertex = new HEVertex(newPoint.x, newPoint.y, newPoint.z);
-        newVertices.push(newVertex);
-        hemesh.vertices.push(newVertex);
-
-        var newFace = new HEFace();
-        newFaces.push(newFace);
-        hemesh.faces.push(newFace);
-
-        for(var j=0; j<3; j++) {
-          var newEdge = new HEEdge();
-          newEdges.push(newEdge);
-          hemesh.edges.push(newEdge);
+        var newVertexPos = edgeToSplit.vert.added(normal.scaled(height));
+        edgeToSplit.vert.edge = edgeToSplit; //TODO: fix that, making sure we split the right face
+        var newEdge = hemesh.splitVertex(edgeToSplit.vert, newVertexPos);
+        newEdges.push(newEdge);
+        if (edgeToSplit == lastEdge) {
+          break;
         }
-        edge = edge.next;
-      } while (edge != face.edge);
+        edgeToSplit = edgeToSplit.next;
+      } while(edgeToSplit != edge);
 
-      edge = face.edge;
-      for(var i=0; i<newFaces.length; i++) {
-        var oldNext = edge.next;
-
-        newEdges[3*i+0].vert = oldNext.vert;
-        newEdges[3*i+1].vert = newVertices[(i+1)%newVertices.length];
-        newEdges[3*i+2].vert = newVertices[i];
-
-        edge.vert.edge = edge;
-        newEdges[3*i+0].vert.edge = newEdges[3*i+0];
-        newEdges[3*i+1].vert.edge = newEdges[3*i+1];
-        newEdges[3*i+2].vert.edge = newEdges[3*i+2];
-
-        edge.next = newEdges[3*i+0];
-        newEdges[3*i+0].next = newEdges[3*i+1];
-        newEdges[3*i+1].next = newEdges[3*i+2];
-        newEdges[3*i+2].next = edge;
-
-        edge.face = newFaces[i];
-        newEdges[3*i+0].face = newFaces[i];
-        newEdges[3*i+1].face = newFaces[i];
-        newEdges[3*i+2].face = newFaces[i];
-
-        newFaces[i].edge = edge;
-
-        edge = oldNext;
+      var prevCornerEdge = newEdges[newEdges.length-1].next;
+      for(var i=0; i<newEdges.length; i++) {
+        var tmp = newEdges[i].next;
+        var newFace = hemesh.splitFace(prevCornerEdge, newEdges[i].next);
+        prevCornerEdge = tmp;
       }
-
-      var topEdges = [];
-      for(var i=0; i<newVertices.length; i++) {
-        var newEdge = new HEEdge();
-        topEdges.push(newEdge);
-        hemesh.edges.push(newEdge);
-      }
-
-      for(var i=0; i<newVertices.length; i++) {
-        topEdges[i].next = topEdges[(i+1)%topEdges.length];
-        topEdges[i].vert = newVertices[i];
-        topEdges[i].face = face;
-      }
-      face.edge = topEdges[0];
     }
-    console.log("end!");
-    hemesh.check();
-    hemesh.assignEdgePairs();
     return hemesh;
   }
 
