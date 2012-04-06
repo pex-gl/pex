c4acbb468d15230733fd9a92bf7bf2d5d9a539cc
Marcin Ignac <marcin.ignac@gmail.com>
2012-04-06 23:34:19 +0200

    Changed Geometry.MAX_VERTICES to what is should be

--- src/pex/core/Geometry.js
+++ src/pex/core/Geometry.js
@@ -37,7 +37,7 @@ define([
   //### Geometry.MAX_VERTICES
   //Maximum number of different vertices per geometry. Limited by the type of 
   //index buffer variables which according to WebGL is a 16 bit integer.
-  Geometry.MAX_VERTICES = 10*65536;
+  Geometry.MAX_VERTICES = 65536;
 
   Geometry.POINTS = 0;
   Geometry.LINES = 1;
