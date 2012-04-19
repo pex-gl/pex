1b8e270fea9dd2ae233332cf093a1ef061bcc9b0
Marcin Ignac <marcin.ignac@gmail.com>
2012-04-19 17:06:31 +0100

    Changed order of transform matrices to S R T

--- src/pex/cameras/PerspectiveCamera.js
+++ src/pex/cameras/PerspectiveCamera.js
@@ -162,9 +162,9 @@ define(["pex/core/Vec3", "pex/core/Vec4", "pex/core/Mat4", "pex/core/Ray"], func
     var s = modelScale ? modelScale : new Vec3(1, 1, 1);
 
     var modelWorldMatrix = new Mat4();
-    modelWorldMatrix.translate(t.x, t.y, t.z);
-    modelWorldMatrix.rotate(r.w, r.x, r.y, r.z);
     modelWorldMatrix.scale(s.x, s.y, s.z);
+    modelWorldMatrix.rotate(r.w, r.x, r.y, r.z);
+    modelWorldMatrix.translate(t.x, t.y, t.z);
 
     var modelViewMatrix = this.viewMatrix.dup();
 
