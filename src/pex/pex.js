bcc06abc52ef79f7d8b14b099d4c42e0ec347736
Marcin Ignac <marcin.ignac@gmail.com>
2012-04-19 18:48:53 +0100

    Added GUI module to PEX module so it builds properly

--- src/pex/pex.js
+++ src/pex/pex.js
@@ -3,15 +3,17 @@ define("pex/Pex", [
     "pex/cameras/Cameras",
     "pex/core/Core",
     "pex/geom/Geom",
+    "pex/gui/GUI",
     "pex/materials/Materials",
     "pex/sys/Sys",
     "pex/util/Util",
   ],
-  function(Cameras, Core, Geom, Materials, Sys, Util) {
+  function(Cameras, Core, Geom, GUI, Materials, Sys, Util) {
     var Pex = {
       Cameras : Cameras,
       Core : Core,
       Geom : Geom,
+      GUI : GUI,
       Materials : Materials,
       Sys : Sys,
       Util : Util
