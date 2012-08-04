850e3fecc9d4e359f4f42f17e4dfae381090435f
vorg <vorg@Marcins-MacBook-Pro-4.local>
2012-08-04 18:02:41 +0200

    ScreenImage texture is now optional (can be bound externally)

--- src/pex/gui/ScreenImage.js
+++ src/pex/gui/ScreenImage.js
@@ -11,7 +11,7 @@ define(["pex/core/Context", "pex/core/Program", "pex/core/Vbo"], function(Contex
   var frag = ""
     + "varying vec2 vTexCoord;"
     + "uniform sampler2D tex0;"
-    + "void main() {"    
+    + "void main() {"
     +   "gl_FragColor = texture2D(tex0, vTexCoord);"
     + "}";
 
@@ -20,36 +20,36 @@ define(["pex/core/Context", "pex/core/Program", "pex/core/Vbo"], function(Contex
     this.program = new Program(vert, frag);
     this.vbo = new Vbo(gl.TRIANGLES);
     this.tex = tex;
-    
+
     var vertices = [
       -1 + 2*x/screenWidth    ,  1 - 2*(y+h)/screenHeight,
       -1 + 2*x/screenWidth    ,  1 - 2*y/screenHeight,
       -1 + 2*(x+w)/screenWidth,  1 - 2*y/screenHeight,
       -1 + 2*(x+w)/screenWidth,  1 - 2*(y+h)/screenHeight
     ];
-    
+
     var texCoords = [
        0,  0,
        0,  1,
        1,  1,
        1,  0
     ];
-    
+
     var indices = [
       0, 1, 2,
       2, 3, 0
     ]
-    
+
     this.vbo.addAttrib("position", vertices, 2);
-    this.vbo.addAttrib("texCoord", texCoords, 2);    
+    this.vbo.addAttrib("texCoord", texCoords, 2);
     this.vbo.setIndices(indices);
   }
-  
+
   ScreenImage.prototype.draw = function(program) {
     var gl = Context.currentContext.gl;
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.bindTexture(gl.TEXTURE_2D, this.tex);    
+    if (this.tex) gl.bindTexture(gl.TEXTURE_2D, this.tex);
     program = program ? program : this.program;
     program.use();
     gl.disable(gl.DEPTH_TEST);
@@ -57,6 +57,6 @@ define(["pex/core/Context", "pex/core/Program", "pex/core/Vbo"], function(Contex
     gl.disable(gl.BLEND);
     gl.enable(gl.DEPTH_TEST);
   }
-  
+
   return ScreenImage;
 });
