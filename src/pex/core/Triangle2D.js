69f6fb0a588067208215a805702ebe3b5e2bb295
vorg <vorg@Marcins-MacBook-Pro-4.local>
2012-08-04 18:32:33 +0200

    Updated Triangle hit testing

--- src/pex/core/Triangle2D.js
+++ src/pex/core/Triangle2D.js
@@ -1,19 +1,24 @@
 define(["pex/core/Line2D"], function(Line2D) {
+  function sign(a, b, c)
+  {
+    //return ((this.b.x - this.a.x)*(p.y - this.a.y) - (this.b.y - this.a.y)*(p.x - this.a.x)) >= 0;
+    return (a.x - c.x) * (b.y - c.y) - (b.x - c.x) * (a.y - c.y);
+  }
+
   function Triangle2D(a, b, c) {
     this.a = a;
     this.b = b;
     this.c = c;
   }
 
-  //Doesn't handle point on the edge
+  //http://stackoverflow.com/a/2049593
+  //doesn't properly handle points on the edge of the triangle
   Triangle2D.prototype.contains = function(p) {
-    var ab = new Line2D(this.a, this.b);
-    var bc = new Line2D(this.b, this.c);
-    var ca = new Line2D(this.c, this.a);
-    var isLeftAB = ab.isPointOnTheLeftSide(p);
-    var isLeftBC = bc.isPointOnTheLeftSide(p);
-    var isLeftCA = ca.isPointOnTheLeftSide(p);
-    return (isLeftAB && isLeftBC && isLeftCA) || (!isLeftAB && !isLeftBC && !isLeftCA);
+    var signAB = sign(this.a, this.b, p) < 0;
+    var signBC = sign(this.b, this.c, p) < 0;
+    var signCA = sign(this.c, this.a, p) < 0;
+
+    return (signAB == signBC) && (signBC == signCA);
   }
 
   return Triangle2D;
