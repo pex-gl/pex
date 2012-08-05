cb8e24fedec9871e6ca61cc5c4ef671cecbc14d6
vorg <vorg@Marcins-MacBook-Pro-4.local>
2012-08-05 03:52:12 +0200

    New Texture2D factory function

--- src/pex/core/Texture2D.js
+++ src/pex/core/Texture2D.js
@@ -16,6 +16,27 @@ define(["pex/core/Texture","pex/core/Context","pex/sys/IO"], function(Texture, C
 
   Texture2D.prototype = new Texture();
 
+  Texture2D.create = function(w, h) {
+    var gl = Context.currentContext.gl;
+
+    var texture = new Texture2D();
+    texture.bind();
+
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null
+    );
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    texture.width = w;
+    texture.height = h;
+
+    return texture;
+  }
+
   //### bind ( unit )
   //Binds the texture to the current GL context.
   //`unit` - texture unit in which to place the texture *{ Number/Int }* = 0
