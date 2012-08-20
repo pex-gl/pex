1543ff1fa7a4c361ed766a5e5b7aae7aeb2d15d9
vorg <vorg@Marcins-MacBook-Pro-4.local>
2012-08-20 13:22:18 +0200

    Added constant for Texture Float in OpenGL

--- src/pex/core/Texture.js
+++ src/pex/core/Texture.js
@@ -1,25 +1,27 @@
 define(["pex/core/Context"], function(Context) {
   function Texture(target) {
     if (target) {
-      console.log("Texture+");      
+      console.log("Texture+");
       this.init(target);
     }
   }
-  
+
+  Texture.RGBA32F = 34836;
+
   Texture.prototype.init = function(target) {
     this.gl = Context.currentContext.gl;
     this.target = target;
-    this.handle = this.gl.createTexture();    
+    this.handle = this.gl.createTexture();
   }
-  
+
   //### bind ( unit )
   //Binds the texture to the current GL context.  
   //`unit` - texture unit in which to place the texture *{ Number/Int }* = 0
   Texture.prototype.bind = function(unit) {
-    unit = unit ? unit : 0;    
+    unit = unit ? unit : 0;
     this.gl.activeTexture(this.gl.TEXTURE0 + unit);
     this.gl.bindTexture(this.target,  this.handle);
   }
-  
+
   return Texture;
 });
