52746c9e0b77b9dad2c36137439ee92b70151fcb
Vorg <marcin.ignac@gmail.com>
2012-11-01 19:23:00 +0000

    Skia GUI renderer layout updates for radio lists

--- src/pex/gui/SkiaRenderer.js
+++ src/pex/gui/SkiaRenderer.js
@@ -57,6 +57,7 @@ define(["plask", "pex/core/Context", "pex/core/Texture2D"], function(plask, Cont
       if (e.type == "slider") eh = 34;
       if (e.type == "button") eh = 24;
       if (e.type == "texture2D") eh = 24 + e.texture.height * w / e.texture.width;
+      if (e.type == "radiolist") eh = 18 + e.items.length * 24;
 
       canvas.drawRect(this.panelBgPaint, dx, dy, dx + w, dy + eh - 2);
 
@@ -83,15 +84,15 @@ define(["plask", "pex/core/Context", "pex/core/Texture2D"], function(plask, Cont
       else if (e.type == "radiolist") {
         canvas.drawText(this.fontPaint, e.title, dx + 3, dy + 13);
         var itemColor = this.controlBgPaint;
+        var itemHeight = 24;
         for(var j=0; j<e.items.length; j++) {
           var item = e.items[j];
           var on = (e.contextObject[e.attributeName] == item.value);
           var itemColor = on ? this.controlHighlightPaint : this.controlBgPaint;
-          canvas.drawRect(itemColor, dx + 3, eh + j*eh + dy + 3, dx + eh - 5, eh + j*eh + dy + eh - 5);
-          canvas.drawText(this.fontPaint, item.name, dx + 5 + eh - 5, eh + j*eh + dy + 13);
+          canvas.drawRect(itemColor, dx + 3, 18 + j*itemHeight + dy + 3, dx + itemHeight - 5, itemHeight + j*itemHeight + dy + 18 - 5);
+          canvas.drawText(this.fontPaint, item.name, dx + 5 + itemHeight - 5, 18 + j*itemHeight + dy + 13);
         }
-        e.activeArea.set(dx + 3, eh + dy + 3, eh - 5, e.items.length * eh - 5);
-        eh = eh + e.items.length * eh;
+        e.activeArea.set(dx + 3, 18 + dy + 3, itemHeight - 5, e.items.length * 24 - 5);
       }
       else if (e.type == "texture2D") {
         canvas.drawText(this.fontPaint, e.title, dx + 3, dy + 13);
