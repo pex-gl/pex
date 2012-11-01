85c902b654260eaa227868833c27a6140df73466
Vorg <marcin.ignac@gmail.com>
2012-11-01 19:19:10 +0000

    Fixed line side detection function

--- src/pex/core/Line2D.js
+++ src/pex/core/Line2D.js
@@ -7,7 +7,7 @@ define([], function() {
 
   //http://stackoverflow.com/questions/3461453/determine-which-side-of-a-line-a-point-lies
   Line2D.prototype.isPointOnTheLeftSide = function(p){
-    return ((this.b.x - this.a.x)*(p.y - this.a.y) - (this.b.y - this.a.y)*(p.x - this.a.x)) >= 0;
+    return ((this.b.x - this.a.x)*(p.y - this.a.y) - (this.b.y - this.a.y)*(p.x - this.a.x)) <= 0;
   }
 
   Line2D.prototype.projectPoint = function(p) {
