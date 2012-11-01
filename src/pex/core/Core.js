738b88c94cce2c5b3e763312cf65bd11968a40ea
Vorg <marcin.ignac@gmail.com>
2012-11-01 19:18:31 +0000

    Updated core package

--- src/pex/core/Core.js
+++ src/pex/core/Core.js
@@ -24,12 +24,15 @@ define( [
   "pex/core/Rect",
   "pex/core/Line2D",
   "pex/core/Triangle2D",
-  "pex/core/RenderTarget"
+  "pex/core/RenderTarget",
+  "pex/core/Polygon2D",
+  "pex/core/FullScreenQuad"
   ],
   function(
       Context, Vec2, Vec3, Vec4, Mat4, Quat, Edge, Face3, Face4, Color,
       Ray, Vbo, Mesh, Program, Material, Texture, Texture2D, TextureCube,
-      Fbo, Geometry, Spline, Rect, Line2D, Triangle2D, RenderTarget
+      Fbo, Geometry, Spline, Rect, Line2D, Triangle2D, RenderTarget,
+      Polygon2D, FullScreenQuad
     ) {
     return {
       Context : Context,
@@ -56,6 +59,8 @@ define( [
       Rect : Rect,
       Line2D : Line2D,
       Triangle2D : Triangle2D,
-      RenderTarget : RenderTarget
+      RenderTarget : RenderTarget,
+      Polygon2D : Polygon2D,
+      FullScreenQuad : FullScreenQuad
     };
 });
