30648aa71fb0c42b36888181a053f55a78fc92fd
Vorg <marcin.ignac@gmail.com>
2012-11-01 19:07:07 +0000

    Added couple of methods to RandUtils

--- src/pex/util/RandUtils.js
+++ src/pex/util/RandUtils.js
@@ -13,8 +13,39 @@ define(["pex/core/Vec2", "pex/core/Vec3", "pex/lib/seedrandom"], function(Vec2, 
     return v;
   }
 
+  //random unit vector on a sphere
+  RandUtils.randomVec3InSphere = function(r) {
+    r = r || 1;
+    var v = new Vec3(
+      Math.random() - 0.5,
+      Math.random() - 0.5,
+      Math.random() - 0.5
+    );
+    v.normalize().scale(Math.random() * r);
+    return v;
+  }
+
+  RandUtils.randomVec3OnSphere = function(r) {
+    r = r || 1;
+    var v = new Vec3(
+      Math.random() - 0.5,
+      Math.random() - 0.5,
+      Math.random() - 0.5
+    );
+    v.normalize().scale(r);
+    return v;
+  }
+
   RandUtils.randomVec2InRect = function(rect) {
     return new Vec2(rect.x + Math.random() * rect.width, rect.y + Math.random() * rect.height);
+  }
+
+  RandUtils.randomInt = function(min, max) {
+    if (max === undefined) {
+      max = min;
+      min = 0;
+    }
+    return Math.floor(min + Math.random() * (max - min));
   }
 
   RandUtils.seed = function(seed) {
