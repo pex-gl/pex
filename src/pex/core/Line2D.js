cc78705b6e960fa9649cfacb6f219e54052608c8
Vorg <marcin.ignac@gmail.com>
2012-11-01 19:19:44 +0000

    Added line intersection test

--- src/pex/core/Line2D.js
+++ src/pex/core/Line2D.js
@@ -1,4 +1,4 @@
-define([], function() {
+define(["pex/core/Vec2"], function(Vec2) {
 
   function Line2D(a, b) {
     this.a = a;
@@ -21,6 +21,36 @@ define([], function() {
   Line2D.prototype.distanceToPoint = function(p) {
     var pOnLine = this.projectPoint(p);
     return pOnLine.distance(p);
+  }
+
+  //this is not completely correct as it test intersection between two segments
+  Line2D.prototype.intersect = function(line) {
+    var sqrEpsilon = 0.000001;
+    var P0 = this.a;
+    var D0 = this.b.subbed(this.a);
+    var P1 = line.a;
+    var D1 = line.b.subbed(line.a);
+
+    var E = P1.subbed(P0);
+
+    var kross = D0.x * D1.y - D0.y * D1.x;
+    var sqrKross = kross * kross;
+    var sqrLen0 = D0.x * D0.x + D0.y * D0.y;
+    var sqrLen1 = D1.x * D1.x + D1.y * D1.y;
+    if (sqrKross > sqrEpsilon * sqrLen0 * sqrLen1) {
+      // lines are not parallel
+      var s = (E.x * D1.y - E.y *D1.x) / kross;
+      return P0.added(D0.scaled(s));
+    }
+    // lines are parallel
+    var sqrLenE = E.x * E.x + E.y * E.y;
+    kross = E.x * D0.y - E.y * D0.x;
+    sqrKross = kross * kross;
+    if (sqrKross > sqrEpsilon * sqrLen0 * sqrLenE) {
+        // lines are different
+        return null;
+    }
+    return null;
   }
 
   return Line2D;
