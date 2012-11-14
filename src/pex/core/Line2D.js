9cf89caf3eb7ee2525b44a595233296a0c76fbd8
Vorg <marcin.ignac@gmail.com>
2012-11-14 14:04:49 +0000

    Whitespace fix

--- src/pex/core/Line2D.js
+++ src/pex/core/Line2D.js
@@ -1,4 +1,4 @@
-define(["pex/core/Vec2"], function(Vec2) {
+  define(["pex/core/Vec2"], function(Vec2) {
 
   function Line2D(a, b) {
     this.a = a;
@@ -9,19 +9,19 @@ define(["pex/core/Vec2"], function(Vec2) {
   //AB cross (AP), or determinant of 2x2 matrix
   Line2D.prototype.isPointOnTheLeftSide = function(p){
     return ((this.b.x - this.a.x)*(p.y - this.a.y) - (this.b.y - this.a.y)*(p.x - this.a.x)) <= 0;
-  }
+  };
 
   Line2D.prototype.projectPoint = function(p) {
     var ab = this.b.subbed(this.a).normalize();
     var ap = p.subbed(this.a);
     var pOnLine = this.a.added(ab.scale(ab.dot(ap)));
     return pOnLine;
-  }
+  };
 
   Line2D.prototype.distanceToPoint = function(p) {
     var pOnLine = this.projectPoint(p);
     return pOnLine.distance(p);
-  }
+  };
 
   //this is not completely correct as it test intersection between two segments
   Line2D.prototype.intersect = function(line) {
