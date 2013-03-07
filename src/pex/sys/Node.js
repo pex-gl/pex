a7801ff2c10bebabf3c7d83e147f96a4cdfd4aaf
Vorg <marcin.ignac@gmail.com>
2013-03-07 16:26:47 +0000

    Single quotes

--- src/pex/sys/Node.js
+++ src/pex/sys/Node.js
@@ -1,4 +1,4 @@
-define(["pex/sys/Platform"], function(Platform) {
+define(['pex/sys/Platform'], function(Platform) {
   var include = (typeof pexNodeRequire === 'function') ? pexNodeRequire : require;
   return {
     plask : Platform.isPlask ? include('plask') : {},
