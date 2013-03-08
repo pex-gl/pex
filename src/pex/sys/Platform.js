5669b182c72632f0fd8b70af3c192b8f557e83b6
Vorg <marcin.ignac@gmail.com>
2013-03-08 00:03:26 +0000

    Ejecta platform check

--- src/pex/sys/Platform.js
+++ src/pex/sys/Platform.js
@@ -1,8 +1,10 @@
 define([], function() {
   var isPlask = (typeof window === 'undefined') && (typeof process === 'object');
   var isBrowser = (typeof window === 'object') && (typeof document === 'object');
+  var isEjecta = (typeof ejecta === 'object') && (typeof ejecta.include === 'function');
   return {
     isPlask : isPlask,
-    isBrowser : isBrowser
+    isBrowser : isBrowser,
+    isEjecta : isEjecta
   }
 });
