47bdab8f96838cad62b9ab100128b449268ed772
Vorg <marcin.ignac@gmail.com>
2013-03-10 19:51:23 +0000

    Object utils

---
+++ src/pex/utils/Object.js
@@ -0,0 +1,21 @@
+define([], function() {
+  function Object() {
+  }
+
+  Object.mergeObjects = function(a, b) {
+    var result = { };
+    if (a) {
+      for(var prop in a) {
+        result[prop] = a[prop];
+      }
+    }
+    if (b) {
+      for(var prop in b) {
+        result[prop] = b[prop];
+      }
+    }
+    return result;
+  }
+
+  return Object;
+});
