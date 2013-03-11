8f7ddce3945cab62ec811ff606aeb52e5da0cce6
Vorg <marcin.ignac@gmail.com>
2013-03-11 17:19:19 +0000

    Added vertex attrib to Geometry

--- src/pex/geom/Geometry.js
+++ src/pex/geom/Geometry.js
@@ -1,6 +1,16 @@
-define([], function() {
+define(['pex/geom/Vec2Array', 'pex/geom/Vec3Array', 'pex/geom/Vec4Array'], function(Vec2Array, Vec3Array, Vec4Array) {
 
-  function Geometry() {
+  function Geometry(attribs) {
+    this.faces = [];
+    this.edges = [];
+    this.attribs = attribs || {};
+
+    for(var attribName in this.attribs) {
+      var attrib = this.attribs[attribName];
+      if (attrib.type == 'Vec2') { attrib.data = new Vec2Array(attrib.length); }
+      if (attrib.type == 'Vec3') { attrib.data = new Vec3Array(attrib.length); }
+      if (attrib.type == 'Vec4') { attrib.data = new Vec4Array(attrib.length); }
+    }
   }
 
   return Geometry;
