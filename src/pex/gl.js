ff1a49d6e333e62e1550865cdafa76392d9f6a6d
Vorg <marcin.ignac@gmail.com>
2013-03-11 23:11:48 +0000

    Added RenderTarget class

--- src/pex/gl.js
+++ src/pex/gl.js
@@ -4,14 +4,16 @@ define(
     'pex/gl/Context',
     'pex/gl/Program',
     'pex/gl/Mesh',
-    'pex/gl/Texture2D'
+    'pex/gl/Texture2D',
+    'pex/gl/RenderTarget'
   ],
-  function(Context, Program, Mesh, Texture2D) {
+  function(Context, Program, Mesh, Texture2D, RenderTarget) {
     return {
       Context : Context,
       Program : Program,
       Mesh : Mesh,
-      Texture2D : Texture2D
+      Texture2D : Texture2D,
+      RenderTarget : RenderTarget
     };
   }
 );
