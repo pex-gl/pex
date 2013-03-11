d7bf49da760db7b5f5d874d933a1550b55868025
Vorg <marcin.ignac@gmail.com>
2013-03-11 00:55:11 +0000

    Updated geom package

--- src/pex/geom.js
+++ src/pex/geom.js
@@ -7,9 +7,11 @@ define(
     'pex/geom/Mat4',
     'pex/geom/Quat',
     'pex/geom/Geometry',
-    'pex/geom/gen'
+    'pex/geom/gen',
+    'pex/geom/Face3',
+    'pex/geom/Face4',
   ],
-  function(Vec2, Vec3, Vec4, Mat3, Mat4, Quat, Geometry, gen) {
+  function(Vec2, Vec3, Vec4, Mat3, Mat4, Quat, Geometry, gen, Face3, Face4) {
     return {
       Vec2 : Vec2,
       Vec3 : Vec4,
@@ -18,7 +20,9 @@ define(
       Mat4 : Mat4,
       Quat : Quat,
       Geometry : Geometry,
-      gen : gen
+      gen : gen,
+      Face3 : Face3,
+      Face4 : Face4
     };
   }
 );
