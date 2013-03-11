108d38209c2dd2479f690aed2b452a73f5940a68
Vorg <marcin.ignac@gmail.com>
2013-03-11 10:36:59 +0000

    Renamed SolidColorMaterial to SolidColor

--- src/pex/materials/SolidColor.js
+++ src/pex/materials/SolidColor.js
@@ -1,6 +1,6 @@
 define(['pex/materials/Material', "pex/gl", "pex/geom", "pex/utils/ObjectUtils"], function(Material, gl, geom, ObjectUtils) {
 
-  var solidColorVert = ""
+  var vert = ""
     + "uniform mat4 projectionMatrix;"
     + "uniform mat4 modelViewMatrix;"
     + "uniform float pointSize;"
@@ -10,14 +10,14 @@ define(['pex/materials/Material', "pex/gl", "pex/geom", "pex/utils/ObjectUtils"]
     +  "gl_PointSize = pointSize;"
     + "}";
 
-  var solidColorFrag = ""
+  var frag = ""
     + "uniform vec4 color;"
     + "void main() {"
     +  "gl_FragColor = color;"
     + "}";
 
 
-  function SolidColorMaterial(uniforms) {
+  function SolidColor(uniforms) {
     this.gl = gl.Context.currentContext.gl;
     var program = new gl.Program(solidColorVert, solidColorFrag);
 
@@ -31,7 +31,7 @@ define(['pex/materials/Material', "pex/gl", "pex/geom", "pex/utils/ObjectUtils"]
     Material.call(this, program, uniforms);
   }
 
-  SolidColorMaterial.prototype = new Material();
+  SolidColor.prototype = new Material();
 
-  return SolidColorMaterial;
+  return SolidColor;
 });
