132ca339115e2e3d7f77e4ca508a8504699e028e
Vorg <marcin.ignac@gmail.com>
2013-03-11 00:55:40 +0000

    Faces and indices support in mesh

--- src/pex/gl/Mesh.js
+++ src/pex/gl/Mesh.js
@@ -1,5 +1,5 @@
-define(['pex/gl/Context', 'pex/geom/Vec3', 'pex/geom/Quat', 'pex/geom/Mat4'],
-function(Context, Vec3, Quat, Mat4) {
+define(['pex/gl/Context', 'pex/geom/Vec3', 'pex/geom/Quat', 'pex/geom/Mat4', 'pex/geom/Face3', 'pex/geom/Face4'],
+function(Context, Vec3, Quat, Mat4, Face3, Face4) {
   function Mesh(geometry, material) {
     this.gl = Context.currentContext.gl;
     this.geometry = geometry;
@@ -19,6 +19,8 @@ function(Context, Vec3, Quat, Mat4) {
     this.modelWorldMatrix = Mat4.create();
     this.modelViewMatrix = Mat4.create();
     this.rotationMatrix = Mat4.create();
+
+    this.updateIndices(geometry);
   }
 
   Mesh.prototype.updateMatrices = function(camera) {
@@ -31,6 +33,33 @@ function(Context, Vec3, Quat, Mat4) {
 
     Mat4.copy(this.modelViewMatrix, camera.getViewMatrix());
     Mat4.mul(this.modelViewMatrix, this.modelViewMatrix, this.modelWorldMatrix);
+  }
+
+  Mesh.prototype.updateIndices = function(geometry) {
+    if (this.indices === undefined) {
+      this.indices = {};
+      this.indices.buffer = this.gl.createBuffer();
+    }
+    var data = [];
+    geometry.faces.forEach(function(face) {
+      if (face instanceof Face4) {
+        data.push(face.a);
+        data.push(face.b);
+        data.push(face.d);
+        data.push(face.d);
+        data.push(face.b);
+        data.push(face.c);
+      }
+      if (face instanceof Face3) {
+        data.push(face.a);
+        data.push(face.b);
+        data.push(face.c);
+      }
+    });
+    this.indices.data = new Uint16Array(data);
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, this.indices.data, this.usage);
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, null);
   }
 
   Mesh.prototype.addAttrib = function(name, data, elementSize, usage) {
@@ -96,21 +125,21 @@ function(Context, Vec3, Quat, Mat4) {
 
     //return;
 
-    //if (this.indices) {
-    //  this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
-    //  this.gl.drawElements(this.primitiveType, this.indices.data.length, this.gl.UNSIGNED_SHORT, 0);
-    //}
-    //else if (this.attributes["position"]){
+    if (this.indices) {
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer);
+      this.gl.drawElements(this.primitiveType, this.indices.data.length, this.gl.UNSIGNED_SHORT, 0);
+    }
+    else if (this.attributes["position"]){
       var num = this.attributes["position"].data.length/3;
       this.gl.drawArrays(this.primitiveType, 0, num);
-    //}
+    }
 
-    //for(var name in this.attributes) {
-    //  var attrib = this.attributes[name];
-    //  if (attrib.location >= 0) {
-    //    this.gl.disableVertexAttribArray(attrib.location);
-    //  }
-    //}
+    for(var name in this.attributes) {
+      var attrib = this.attributes[name];
+      if (attrib.location >= 0) {
+        this.gl.disableVertexAttribArray(attrib.location);
+      }
+    }
   }
 
   return Mesh;
