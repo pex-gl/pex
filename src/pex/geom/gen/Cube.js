af5965a450208c31bc41811deca113527c81bca9
Vorg <marcin.ignac@gmail.com>
2013-03-11 17:19:57 +0000

    Using Geometry attribs in Cube gen

--- src/pex/geom/gen/Cube.js
+++ src/pex/geom/gen/Cube.js
@@ -1,7 +1,6 @@
 define(['pex/geom/Vec2', 'pex/geom/Vec3', 'pex/geom/Vec2Array', 'pex/geom/Vec3Array', 'pex/geom/Face4', 'pex/geom/Geometry'],
   function(Vec2, Vec3, Vec2Array, Vec3Array, Face4, Geometry) {
   function Cube(sx, sy, sz, nx, ny, nz) {
-    Geometry.call(this);
     sx = sx || 1;
     sy = sy || sx || 1;
     sz = sz || sx || 1;
@@ -12,37 +11,46 @@ define(['pex/geom/Vec2', 'pex/geom/Vec3', 'pex/geom/Vec2Array', 'pex/geom/Vec3Ar
     var numVertices = (nx + 1) * (ny + 1) * 2 + (nx + 1) * (nz + 1) * 2 + (nz + 1) * (ny + 1) * 2;
     var vertexIndex = 0;
 
-    this.attribs = [];
-    var vertices = this.vertices = [];
-    var texCoords = this.texCoords = [];
-    var normals = this.normals = [];
-    var faces = this.faces = [];
+    var attribs = {
+      position : {
+        type : 'Vec3',
+        length : numVertices
+      },
+      normal : {
+        type : 'Vec3',
+        length : numVertices
+      },
+      texCoord : {
+        type : 'Vec2',
+        length : numVertices
+      }
+    });
 
-    var positionAttrib = new Vec3Array(numVertices);
-    var texCoordAttrib = new Vec3Array(numVertices);
-    var normalAttrib = new Vec3Array(numVertices);
+    Geometry.call(this, attribs);
 
-    this.positionAttrib = positionAttrib;
-    this.texCoordAttrib = texCoordAttrib;
-    this.normalAttrib = normalAttrib;
+    var faces = this.faces;
+
+    var positions = this.attribs.position.data;
+    var normals = this.attribs.normal.data;
+    var texCoords = this.attribs.texCoord.data;
 
     function makePlane(u, v, w, su, sv, nu, nv, pw, flipu, flipv) {
       var vertShift = vertexIndex;
       for(var j=0; j<=nv; ++j) {
         for(var i=0; i<=nu; ++i) {
-          var vert = positionAttrib[vertexIndex];
+          var vert = positions[vertexIndex];
           vert[u] = (-su/2 + i*su/nu) * flipu;
           vert[v] = (-sv/2 + j*sv/nv) * flipv;
           vert[w] = pw;
 
-          var texCoord = texCoordAttrib[vertexIndex];
-          texCoord[0] = i/nu;
-          texCoord[1] = j/nv;
-
-          var normal = normalAttrib[vertexIndex];
+          var normal = normals[vertexIndex];
           normal[u] = 0;
           normal[v] = 0;
           normal[w] = pw/Math.abs(pw);
+
+          var texCoord = texCoords[vertexIndex];
+          texCoord[0] = i/nu;
+          texCoord[1] = j/nv;
 
           ++vertexIndex;
         }
