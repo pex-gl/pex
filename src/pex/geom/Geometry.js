718e54a71c510829d527d773be8a5354e35792cd
Vorg <marcin.ignac@gmail.com>
2013-03-11 17:20:49 +0000

    Added isDirty attribute to all the vertex attribs

--- src/pex/geom/Geometry.js
+++ src/pex/geom/Geometry.js
@@ -7,6 +7,7 @@ define(['pex/geom/Vec2Array', 'pex/geom/Vec3Array', 'pex/geom/Vec4Array'], funct
 
     for(var attribName in this.attribs) {
       var attrib = this.attribs[attribName];
+      attrib.isDirty = true;
       if (attrib.type == 'Vec2') { attrib.data = new Vec2Array(attrib.length); }
       if (attrib.type == 'Vec3') { attrib.data = new Vec3Array(attrib.length); }
       if (attrib.type == 'Vec4') { attrib.data = new Vec4Array(attrib.length); }
