a2c445a304d6a486191028cf91c1802f12e94b1c
Vorg <marcin.ignac@gmail.com>
2013-03-11 20:47:10 +0000

    Moved Textured material shader code to separate file

--- src/pex/materials/Textured.js
+++ src/pex/materials/Textured.js
@@ -1,32 +1,18 @@
-define(['pex/gl/Context', 'pex/materials/Material', 'pex/gl/Program', 'pex/utils/ObjectUtils'],
-  function(Context, Material, Program, ObjectUtils) {
-
-  var vert = ''
-    + 'uniform mat4 projectionMatrix;'
-    + 'uniform mat4 modelViewMatrix;'
-    + 'attribute vec3 position;'
-    + 'attribute vec2 texCoord;'
-    + 'varying vec2 vTexCoord;'
-    + 'void main() {'
-    +  'gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);'
-    +  'gl_PointSize = 2.0;'
-    +  'vTexCoord = texCoord;'
-    + '}';
-
-  var frag = ''
-    + 'uniform sampler2D texture;'
-    + 'varying vec2 vTexCoord;'
-    + 'void main() {'
-    +  'gl_FragColor = texture2D(texture, vTexCoord);'
-    + '}';
-
+define([
+  'pex/gl/Context',
+  'pex/materials/Material',
+  'pex/gl/Program',
+  'pex/utils/ObjectUtils',
+  'text!pex/materials/Textured.glsl'
+  ],
+  function(Context, Material, Program, ObjectUtils, TexturedGLSL) {
 
   function Textured(uniforms) {
-      this.gl = Context.currentContext.gl;
-      var program = new Program(vert, frag);
-      var uniforms = ObjectUtils.mergeObjects({}, uniforms);
+    this.gl = Context.currentContext.gl;
+    var program = new Program(TexturedGLSL);
+    var uniforms = ObjectUtils.mergeObjects({}, uniforms);
 
-      Material.call(this, program, uniforms);
+    Material.call(this, program, uniforms);
   }
 
   Textured.prototype = Object.create(Material.prototype);
