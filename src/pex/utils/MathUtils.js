622b64ab35111e0162208b9a22ee87e21039dfc9
Vorg <marcin.ignac@gmail.com>
2013-03-22 15:59:53 +0100

    Added randomVec2InRect

--- src/pex/utils/MathUtils.js
+++ src/pex/utils/MathUtils.js
@@ -1,10 +1,13 @@
-define(['lib/seedrandom.js'], function(seedrandom) {
+define(['lib/seedrandom.js', 'pex/geom/Vec2'], function(seedrandom, Vec2) {
   function MathUtils() {
   }
 
   MathUtils.seed = function(s) {
     Math.seedrandom(s);
   }
+
+  MathUtils.randomVec2InRect = function(rect) {
+    return Vec2.fromValues(rect.x + Math.random() * rect.width, rect.y + Math.random() * rect.height);
   }
 
   return MathUtils;
