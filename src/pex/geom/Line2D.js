8fdcfca428765082267da707e2960b9de2a9ca25
Vorg <marcin.ignac@gmail.com>
2013-03-22 18:31:55 +0100

    Implemented Line2D intersect

--- src/pex/geom/Line2D.js
+++ src/pex/geom/Line2D.js
@@ -32,33 +32,40 @@
     return Vec2.distance(p, pOnLine);
   };
 
-  Line2D.prototype.intersect = function(line) {
-    //var sqrEpsilon = 0.000001;
-    //var P0 = this.a;
-    //var D0 = this.b.subbed(this.a);
-    //var P1 = line.a;
-    //var D1 = line.b.subbed(line.a);
-//
-    //var E = P1.subbed(P0);
-//
-    //var kross = D0.x * D1.y - D0.y * D1.x;
-    //var sqrKross = kross * kross;
-    //var sqrLen0 = D0.x * D0.x + D0.y * D0.y;
-    //var sqrLen1 = D1.x * D1.x + D1.y * D1.y;
-    //if (sqrKross > sqrEpsilon * sqrLen0 * sqrLen1) {
-    //  // lines are not parallel
-    //  var s = (E.x * D1.y - E.y *D1.x) / kross;
-    //  return P0.added(D0.scaled(s));
-    //}
-    //// lines are parallel
-    //var sqrLenE = E.x * E.x + E.y * E.y;
-    //kross = E.x * D0.y - E.y * D0.x;
-    //sqrKross = kross * kross;
-    //if (sqrKross > sqrEpsilon * sqrLen0 * sqrLenE) {
-    //    // lines are different
-    //    return null;
-    //}
-    //return null;
+  Line2D.prototype.intersect = function(out, line) {
+    var sqrEpsilon = 0.000001;
+    var P0 = this.a;
+    var D0 = Vec2.create();
+    Vec2.sub(D0, this.b, this.a);
+    var P1 = line.a;
+    var D1 = Vec2.create();
+    Vec2.sub(D1, line.b, line.a);
+
+    var E = Vec2.create();
+    Vec2.sub(E, P1, P0);
+
+    var kross = D0[0] * D1[1] - D0[1] * D1[0];
+    var sqrKross = kross * kross;
+    var sqrLen0 = D0[0] * D0[0] + D0[1] * D0[1];
+    var sqrLen1 = D1[0] * D1[0] + D1[1] * D1[1];
+    if (sqrKross > sqrEpsilon * sqrLen0 * sqrLen1) {
+      // lines are not parallel
+      var s = (E[0] * D1[1] - E[1] *D1[0]) / kross;
+      var scaled = Vec2.create();
+      Vec2.scale(scaled, D0, s);
+      Vec2.copy(out, P0);
+      Vec2.add(out, out, scaled);
+      return true;
+    }
+    // lines are parallel
+    var sqrLenE = E[0] * E[0] + E[1] * E[1];
+    kross = E[0] * D0[1] - E[1] * D0[0];
+    sqrKross = kross * kross;
+    if (sqrKross > sqrEpsilon * sqrLen0 * sqrLenE) {
+        // lines are different
+        return false;
+    }
+    return false;
   }
 
   return Line2D;
