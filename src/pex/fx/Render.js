4bf84fc81512dc41085739b7e70c27139bca8985
Vorg <marcin.ignac@gmail.com>
2013-03-26 17:25:14 +0000

    Removed console.log

--- src/pex/fx/Render.js
+++ src/pex/fx/Render.js
@@ -2,7 +2,6 @@ define(['pex/fx/FXGraph'], function(FXGraph) {
   FXGraph.prototype.render = function(options) {
     var gl = this.gl;
     var outputSize = this.getOutputSize(options.width, options.height);
-    console.log(outputSize);
     var rt = this.getRenderTarget(outputSize.width, outputSize.height, options.depth, options.bpp);
     var oldViewport = gl.getParameter(gl.VIEWPORT);
     gl.viewport(0, 0, outputSize.width, outputSize.height);
