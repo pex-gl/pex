19e61df43917493606515a53e89bdc18e5d4083d
Vorg <marcin.ignac@gmail.com>
2013-03-28 16:53:23 +0000

    Updated fx package

--- src/pex/fx.js
+++ src/pex/fx.js
@@ -4,19 +4,21 @@ define(
     'pex/fx/Render',
     'pex/fx/Blit',
     'pex/fx/Downsample2',
-    'pex/fx/Downsample4'
+    'pex/fx/Downsample4',
+    'pex/fx/Blur3',
+    'pex/fx/Blur5',
+    'pex/fx/Blur7'
   ],
-  function(FXGraph, Render, Blit, Downsample2, Downsample4) {
+  function(FXGraph, Render, Blit, Downsample2, Downsample4, Blur3, Blur5, Blur7) {
     var globalFx;
-    return function(source) {
+    return function(reset) {
+      if (typeof reset === undefined) reset = true;
+
       if (!globalFx) {
         globalFx = new FXGraph();
       }
-
-      if (source) {
-        globalFx.use(source);
-      }
-      else {
+      else if (reset) {
+        console.log('resetting');
         globalFx.reset();
       }
       return globalFx;
