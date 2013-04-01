7536f2f9af129d8ebe973d0ca8cfe1dc764b00d8
Vorg <marcin.ignac@gmail.com>
2013-04-01 14:26:40 +0100

    Added Image effect

--- src/pex/fx.js
+++ src/pex/fx.js
@@ -9,9 +9,10 @@ define(
     'pex/fx/Blur5',
     'pex/fx/Blur7',
     'pex/fx/Add',
-    'pex/fx/Threshold'
+    'pex/fx/Threshold',
+    'pex/fx/Image'
   ],
-  function(FXStage, Render, Blit, Downsample2, Downsample4, Blur3, Blur5, Blur7, Add, Threshold) {
+  function(FXStage, Render, Blit, Downsample2, Downsample4, Blur3, Blur5, Blur7, Add, Threshold, Image) {
     var globalFx;
     return function() {
       if (!globalFx) {
