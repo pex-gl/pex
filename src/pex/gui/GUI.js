39e4cec3f9475c235d31d05cc0bdc6b1e85e04c1
Vorg <marcin.ignac@gmail.com>
2013-04-04 15:32:01 +0100

    Added GUI class

---
+++ src/pex/gui/GUI.js
@@ -0,0 +1,376 @@
+define([
+  'pex/gl/Context',
+  'pex/gl/ScreenImage',
+  'pex/utils/Time',
+  'pex/gui/SkiaRenderer',
+  //'pex/gui/HTMLCanvasRenderer',
+  //'pex/gui/NodeCanvasRenderer',
+  'pex/geom/Rect',
+  'pex/sys/IO',
+  'pex/sys/Platform',
+  'pex/geom/Vec2'
+],
+function(Context, ScreenImage, Time, SkiaRenderer, Rect, IO, Platform, Vec2) {
+  
+  function GUIControl(o) {
+    for(var i in o) {
+      this[i] = o[i];
+    }
+  }
+
+  GUIControl.prototype.setPosition = function(x, y) {
+    this.px = x;
+    this.py = y;
+  }
+
+  GUIControl.prototype.getNormalizedValue = function(idx) {
+    if (!this.contextObject) return 0;
+
+    var val = this.contextObject[this.attributeName];
+
+    var options = this.options;
+    if (options && options.min !== undefined && options.max !== undefined) {
+      if (this.type == 'multislider') {
+        val = (val[idx] - options.min) / (options.max - options.min);
+      }
+      else {
+        val = (val - options.min) / (options.max - options.min);
+      }
+    }
+    return val;
+  }
+
+  GUIControl.prototype.setNormalizedValue = function(val, idx) {
+    if (!this.contextObject) return;
+
+    var options = this.options;
+    if (options && options.min !== undefined && options.max !== undefined) {
+      if (this.type == 'multislider') {
+        var a = this.contextObject[this.attributeName];
+        if (idx >= a.length) {
+          return;
+        }
+        a[idx] = options.min + val * (options.max - options.min);
+        val = a;
+      }
+      else {
+        val = options.min + val * (options.max - options.min);
+      }
+    }
+    this.contextObject[this.attributeName] = val;
+  }
+
+  GUIControl.prototype.getValue = function() {
+    if (this.type == 'slider') {
+      return this.contextObject[this.attributeName];
+    }
+    if (this.type == 'multislider') {
+      return this.contextObject[this.attributeName];
+    }
+    else if (this.type == 'toggle') {
+      return this.contextObject[this.attributeName];
+    }
+    else return 0;
+  }
+
+  GUIControl.prototype.getStrValue = function() {
+    if (this.type == 'slider') {
+      var str = '' + this.contextObject[this.attributeName];
+      var dotPos = str.indexOf('.') + 1;
+      while(str.charAt(dotPos) == '0') {
+        dotPos++;
+      }
+      return str.substr(0, dotPos+2);
+    }
+    else if (this.type == 'toggle') {
+      return this.contextObject[this.attributeName];
+    }
+    else return '';
+  }
+
+  function GUI(window, x, y) {
+    this.gl = Context.currentContext.gl;
+    this.window = window;
+    this.x = (x == undefined) ? 0 : x;
+    this.y = (y == undefined) ? 0 : y;
+    this.mousePos = Vec2.create();
+
+    if (Platform.isPlask) {
+      this.renderer = new SkiaRenderer(window.width, window.height);
+    }
+    //else if (IO.Image) {
+      //this.renderer = new NodeCanvasRenderer(window.width, window.height);
+    //}
+    //else {
+      //this.renderer = new HTMLCanvasRenderer(window.width, window.height);
+    //}
+    this.screenBounds = new Rect(this.x, this.y, window.width, window.height);
+    this.screenImage = new ScreenImage(this.renderer.getTexture(), this.x, this.y, window.width, window.height, window.width, window.height);
+
+    this.items = [];
+
+    this.bindEventListeners(window);
+  }
+
+  GUI.prototype.bindEventListeners = function(window) {
+    var self = this;
+    window.on('leftMouseDown', function(e) {
+      self.onMouseDown(e);
+    });
+
+    window.on('mouseDragged', function(e) {
+      self.onMouseDrag(e);
+    });
+
+    window.on('leftMouseUp', function(e) {
+      self.onMouseUp(e);
+    });
+  }
+
+  GUI.prototype.onMouseDown = function(e) {
+    this.activeControl = null;
+    Vec2.set(this.mousePos, e.x - this.x, e.y - this.y);
+    for(var i=0; i<this.items.length; i++) {
+      if (this.items[i].activeArea.contains(this.mousePos)) {
+        this.activeControl = this.items[i];
+        this.activeControl.active = true;
+        this.activeControl.dirty = true;
+        if (this.activeControl.type == 'button') {
+          this.activeControl.contextObject[this.activeControl.methodName]();
+        }
+        else if (this.activeControl.type == 'toggle') {
+          this.activeControl.contextObject[this.activeControl.attributeName] = !this.activeControl.contextObject[this.activeControl.attributeName];
+          if (this.activeControl.onchange) {
+            this.activeControl.onchange(this.activeControl.contextObject[this.activeControl.attributeName]);
+          }
+        }
+        else if (this.activeControl.type == 'radiolist') {
+          var hitY = this.mousePos[1] - this.activeControl.activeArea.y;
+          var hitItemIndex = Math.floor(this.activeControl.items.length * hitY/this.activeControl.activeArea.height);
+          if (hitItemIndex < 0) continue;
+          if (hitItemIndex >= this.activeControl.items.length) continue;
+          this.activeControl.contextObject[this.activeControl.attributeName] = this.activeControl.items[hitItemIndex].value;
+          if (this.activeControl.onchange) {
+            this.activeControl.onchange(this.activeControl.items[hitItemIndex].value);
+          }
+        }
+        e.handled = true;
+        this.onMouseDrag(e);
+        break;
+      }
+    }
+  }
+
+  GUI.prototype.onMouseDrag = function(e) {
+    if (this.activeControl) {
+      var aa = this.activeControl.activeArea;
+      if (this.activeControl.type == 'slider') {
+        var val = (e.x - aa.x) / aa.width;
+        val = Math.max(0, Math.min(val, 1));
+        this.activeControl.setNormalizedValue(val);
+        if (this.activeControl.onchange) {
+          this.activeControl.onchange(this.activeControl.contextObject[this.activeControl.attributeName]);
+        }
+        this.activeControl.dirty = true;
+      }
+      else if (this.activeControl.type == 'multislider') {
+        var val = (e.x - aa.x) / aa.width;
+        val = Math.max(0, Math.min(val, 1));
+        var idx = Math.floor(this.activeControl.getValue().length * (e.y - aa.y) / aa.height);
+        this.activeControl.setNormalizedValue(val, idx);
+        if (this.activeControl.onchange) {
+          this.activeControl.onchange(this.activeControl.contextObject[this.activeControl.attributeName]);
+        }
+        this.activeControl.dirty = true;
+      }
+      e.handled = true;
+    }
+  }
+
+  GUI.prototype.onMouseUp = function(e) {
+    if (this.activeControl) {
+      this.activeControl.active = false;
+      this.activeControl.dirty = true;
+      this.activeControl = null;
+    }
+  }
+
+  GUI.prototype.addLabel = function(title) {
+    var ctrl = new GUIControl(
+      { type: 'label', title: title, dirty : true,activeArea: new Rect(0, 0, 0, 0), setTitle: function(title) { this.title = title; this.dirty = true; } }
+    );
+    this.items.push(ctrl);
+    return ctrl;
+  }
+
+  GUI.prototype.addParam = function(title, contextObject, attributeName, options, onchange) {
+    options = options || {};
+    if (contextObject[attributeName] instanceof Array) {
+      var ctrl = new GUIControl(
+        {
+          type: 'multislider',
+          title: title,
+          contextObject: contextObject,
+          attributeName: attributeName,
+          activeArea: new Rect(0, 0, 0, 0),
+          options: options,
+          onchange : onchange,
+          dirty : true
+        }
+      );
+      this.items.push(ctrl);
+      return ctrl;
+    }
+    else if (contextObject[attributeName] === false || contextObject[attributeName] === true) {
+      var ctrl = new GUIControl(
+        {
+          type: 'toggle',
+          title: title,
+          contextObject: contextObject,
+          attributeName: attributeName,
+          activeArea: new Rect(0, 0, 0, 0),
+          options: options,
+          onchange : onchange,
+          dirty : true
+        }
+      );
+      this.items.push(ctrl);
+      return ctrl;
+    }
+    else {
+      var ctrl = new GUIControl(
+        {
+          type: 'slider',
+          title: title,
+          contextObject: contextObject,
+          attributeName: attributeName,
+          activeArea: new Rect(0, 0, 0, 0),
+          options: options,
+          onchange : onchange,
+          dirty : true
+        }
+      );
+      this.items.push(ctrl);
+      return ctrl;
+    }
+  }
+
+  GUI.prototype.addButton = function(title, contextObject, methodName, options) {
+    var ctrl = new GUIControl(
+      {
+        type: 'button',
+        title: title,
+        contextObject: contextObject,
+        methodName: methodName,
+        activeArea: new Rect(0, 0, 0, 0),
+        dirty : true,
+        options : options
+      }
+    );
+    this.items.push(ctrl);
+    return ctrl;
+  }
+
+  GUI.prototype.addRadioList = function(title, contextObject, attributeName, items, onchange) {
+    var ctrl = new GUIControl(
+      {
+        type: 'radiolist',
+        title: title,
+        contextObject: contextObject,
+        attributeName: attributeName,
+        activeArea: new Rect(0, 0, 0, 0),
+        items: items,
+        onchange : onchange,
+        dirty : true
+      }
+    );
+    this.items.push(ctrl);
+    return ctrl;
+  }
+
+  GUI.prototype.addTexture2D = function(title, texture) {
+    var ctrl = new GUIControl(
+      {
+        type: 'texture2D',
+        title: title,
+        texture: texture,
+        activeArea: new Rect(0, 0, 0, 0),
+        dirty : true
+      }
+    );
+    this.items.push(ctrl);
+    return ctrl;
+  }
+
+  GUI.prototype.dispose = function() {
+    //TODO: delete texture object
+  }
+
+  var frame = 0;
+  GUI.prototype.draw = function() {
+    if (this.items.length == 0) {
+      return;
+    }
+    this.renderer.draw(this.items);
+    //if (!IO.Image)
+    this.screenImage.draw(this.renderer.getTexture());
+    //this.drawTextures();
+  }
+
+  //GUI.prototype.drawTextures = function() {
+  //  for(var i=0; i<this.items.length; i++) {
+  //    var item = this.items[i];
+  //    if (item.type == 'texture2D') {
+  //      if (item.texture.bind) item.texture.bind();
+  //      else {
+  //        this.gl.bindTexture(item.texture.target, item.texture.handle);
+  //      }
+  //      var bounds;
+  //      if (item.texture.flipped) {
+  //        bounds  = new Rect(item.activeArea.x, this.window.height - item.activeArea.y, item.activeArea.width, -item.activeArea.height);
+  //      }
+  //      else {
+  //        bounds = new Rect(item.activeArea.x, this.window.height - item.activeArea.y - item.activeArea.height, item.activeArea.width, item.activeArea.height);
+  //      }
+  //      this.screenImage.setBounds(bounds);
+  //      this.screenImage.setTexture(null);
+  //      this.screenImage.draw();
+  //    }
+  //  }
+  //  this.screenImage.setBounds(this.screenBounds);
+  //  this.screenImage.setTexture(this.renderer.getTexture());
+  //}
+
+  GUI.prototype.serialize = function() {
+    var data = {};
+    this.items.forEach(function(item, i) {
+      data[item.title] = item.getNormalizedValue();
+    })
+    return data;
+  }
+
+  GUI.prototype.deserialize = function(data) {
+    this.items.forEach(function(item, i) {
+      if (!(data[item.title] == undefined)) {
+        item.setNormalizedValue(data[item.title]);
+      }
+    })
+  }
+
+  GUI.prototype.save = function(path) {
+    var data = this.serialize();
+    IO.saveTextFile(path, JSON.stringify(data));
+  }
+
+  GUI.prototype.load = function(path) {
+    var self = this;
+    IO.loadTextFile(path, function(dataStr) {
+      var data = JSON.parse(dataStr);
+      self.deserialize(data);
+    })
+  }
+
+  GUI.ScreenImage = ScreenImage;
+
+  return GUI;
+});
---
+++ src/pex/gui/SkiaRenderer.js
@@ -0,0 +1,158 @@
+  define(['pex/sys/Node', 'pex/gl/Context', 'pex/gl/Texture2D'], function(Node, Context, Texture2D) {
+  var plask = Node.plask
+  function SkiaRenderer(width, height) {
+    this.gl = Context.currentContext.gl;
+    this.tex = Texture2D.create(0, 0);
+    this.canvas = new plask.SkCanvas.create(width, height);
+
+    this.fontPaint = new plask.SkPaint();
+    this.fontPaint.setStyle(plask.SkPaint.kFillStyle);
+    this.fontPaint.setColor(255, 255, 255, 255);
+    this.fontPaint.setTextSize(10);
+    this.fontPaint.setFontFamily('Monaco');
+    this.fontPaint.setStrokeWidth(0);
+
+    this.fontHighlightPaint = new plask.SkPaint();
+    this.fontHighlightPaint.setStyle(plask.SkPaint.kFillStyle);
+    this.fontHighlightPaint.setColor(100, 100, 100, 255);
+    this.fontHighlightPaint.setTextSize(10);
+    this.fontHighlightPaint.setFontFamily('Monaco');
+    this.fontHighlightPaint.setStrokeWidth(0);
+
+    this.panelBgPaint = new plask.SkPaint();
+    this.panelBgPaint.setStyle(plask.SkPaint.kFillStyle);
+    this.panelBgPaint.setColor(0, 0, 0, 150);
+
+    this.controlBgPaint = new plask.SkPaint();
+    this.controlBgPaint.setStyle(plask.SkPaint.kFillStyle);
+    this.controlBgPaint.setColor(150, 150, 150, 255);
+
+    this.controlHighlightPaint = new plask.SkPaint();
+    this.controlHighlightPaint.setStyle(plask.SkPaint.kFillStyle);
+    this.controlHighlightPaint.setColor(255, 255, 0, 255);
+
+    this.controlFeaturePaint = new plask.SkPaint();
+    this.controlFeaturePaint.setStyle(plask.SkPaint.kFillStyle);
+    this.controlFeaturePaint.setColor(255, 255, 255, 255);
+  }
+
+  SkiaRenderer.prototype.isAnyItemDirty = function(items) {
+    var dirty = false;
+    items.forEach(function(item) {
+      if (item.dirty) {
+        item.dirty = false;
+        dirty = true;
+      }
+    });
+    return dirty;
+  };
+
+  SkiaRenderer.prototype.draw = function(items) {
+    if (!this.isAnyItemDirty(items)) {
+      return;
+    }
+
+    console.log('SkiaRenderer.draw');
+
+    var canvas = this.canvas;
+
+    canvas.drawColor(0, 0, 0, 0, plask.SkPaint.kClearMode); //transparent
+
+    var dy = 10;
+    var dx = 10;
+    var w = 160;
+    for(var i=0; i<items.length; i++) {
+      var e = items[i];
+
+      if (e.px && e.px) {
+        dx = e.px;
+        dy = e.py;
+      }
+      var eh = 20;
+
+      if (e.type == "slider") eh = 34;
+      if (e.type == "multislider") eh = 18 + e.getValue().length * 20;
+      if (e.type == "button") eh = 24;
+      if (e.type == "texture2D") eh = 24 + e.texture.height * w / e.texture.width;
+      if (e.type == "radiolist") eh = 18 + e.items.length * 20;
+
+      canvas.drawRect(this.panelBgPaint, dx, dy, dx + w, dy + eh - 2);
+
+      if (e.type == "slider") {
+        var value = e.getValue();
+        console.log(value);
+        canvas.drawRect(this.controlBgPaint, dx + 3, dy + 18, dx + w - 3, dy + eh - 5);
+        canvas.drawRect(this.controlHighlightPaint, dx + 3, dy + 18, dx + 3 + (w - 6)*e.getNormalizedValue(), dy + eh - 5);
+        e.activeArea.set(dx + 3, dy + 18, w - 3 - 3, eh - 5 - 18);
+        canvas.drawText(this.fontPaint, items[i].title + " : " + e.getStrValue(), dx + 3, dy + 13);
+      }
+      else if (e.type == "multislider") {
+        for(var j=0; j<e.getValue().length; j++) {
+          canvas.drawRect(this.controlBgPaint, dx + 3, dy + 18 + (j)*20, dx + w - 3, dy + 18 + (j+1)*20 - 6);
+          canvas.drawRect(this.controlHighlightPaint, dx + 3, dy + 18 + (j)*20, dx + 3 + (w - 6)*e.getNormalizedValue(j), dy + 18 + (j+1)*20 - 6);
+        }
+        canvas.drawText(this.fontPaint, items[i].title + " : " + e.getStrValue(), dx + 3, dy + 13);
+        e.activeArea.set(dx + 3, dy + 18, w - 3 - 3, eh - 5 - 18);
+      }
+      else if (e.type == "button") {
+        var btnColor = e.active ? this.controlHighlightPaint : this.controlBgPaint;
+        var btnFont = e.active ? this.fontHighlightPaint : this.fontPaint;
+        canvas.drawRect(btnColor, dx + 3, dy + 3, dx + w - 3, dy + eh - 5);
+        e.activeArea.set(dx + 3, dy + 3, w - 3 - 3, eh - 5);
+        if (e.options.color) {
+          var c = e.options.color;
+          this.controlFeaturePaint.setColor(255 * c.x, 255 * c.y, 255 * c.z, 255);
+          canvas.drawRect(this.controlFeaturePaint,  dx + w - 8, dy + 3, dx + w - 3, dy + eh - 5);
+        }
+        canvas.drawText(btnFont, items[i].title, dx + 5, dy + 15);
+      }
+      else if (e.type == "toggle") {
+        var on = e.contextObject[e.attributeName];
+        var toggleColor = on ? this.controlHighlightPaint : this.controlBgPaint;
+        canvas.drawRect(toggleColor, dx + 3, dy + 3, dx + eh - 5, dy + eh - 5);
+        e.activeArea.set(dx + 3, dy + 3, eh - 5, eh - 5);
+        canvas.drawText(this.fontPaint, items[i].title, dx + 5 + eh - 5, dy + 13);
+      }
+      else if (e.type == "radiolist") {
+        canvas.drawText(this.fontPaint, e.title, dx + 3, dy + 13);
+        var itemColor = this.controlBgPaint;
+        var itemHeight = 20;
+        for(var j=0; j<e.items.length; j++) {
+          var item = e.items[j];
+          var on = (e.contextObject[e.attributeName] == item.value);
+          var itemColor = on ? this.controlHighlightPaint : this.controlBgPaint;
+          canvas.drawRect(itemColor, dx + 3, 18 + j*itemHeight + dy + 3, dx + itemHeight - 5, itemHeight + j*itemHeight + dy + 18 - 5);
+          canvas.drawText(this.fontPaint, item.name, dx + 5 + itemHeight - 5, 18 + j*itemHeight + dy + 13);
+        }
+        e.activeArea.set(dx + 3, 18 + dy + 3, itemHeight - 5, e.items.length * itemHeight - 5);
+      }
+      else if (e.type == "texture2D") {
+        canvas.drawText(this.fontPaint, e.title, dx + 3, dy + 13);
+        e.activeArea.set(dx + 3, dy + 18, w - 3 - 3, eh - 5 - 18);
+      }
+      else {
+        canvas.drawText(this.fontPaint, items[i].title, dx + 3, dy + 13);
+      }
+      dy += eh;
+    }
+
+    this.updateTexture();
+  }
+
+  SkiaRenderer.prototype.getTexture = function() {
+    return this.tex;
+  }
+
+  SkiaRenderer.prototype.updateTexture = function() {
+    var gl = this.gl;
+    this.tex.bind();
+    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texImage2DSkCanvas(gl.TEXTURE_2D, 0, this.canvas);
+    //gl.generateMipmap(gl.TEXTURE_2D);
+    //gl.bindTexture(gl.TEXTURE_2D, null);
+  }
+
+
+  return SkiaRenderer;
+});
