e66e678fdb337e8ae2ec3a49dcd4a076de4f6a81
Vorg <marcin.ignac@gmail.com>
2013-04-07 21:46:22 +0100

    Updated geom package

--- src/pex/geom.js
+++ src/pex/geom.js
@@ -10,6 +10,7 @@ define(
     'pex/geom/gen',
     'pex/geom/Face3',
     'pex/geom/Face4',
+    'pex/geom/FacePolygon',
     'pex/geom/Vec2Array',
     'pex/geom/Vec3Array',
     'pex/geom/Vec4Array',
@@ -17,9 +18,13 @@ define(
     'pex/geom/Rect',
     'pex/geom/Triangle2D',
     'pex/geom/Polygon2D',
+    'pex/geom/hem',
+    'pex/geom/BoundingBox',
+    'pex/geom/Octree',
   ],
   function(Vec2, Vec3, Vec4, Mat3, Mat4, Quat, Geometry, gen,
-    Face3, Face4, Vec2Array, Vec3Array, Vec4Array, Line2D, Rect, Triangle2D, Polygon2D) {
+    Face3, Face4, FacePolygon, Vec2Array, Vec3Array, Vec4Array, Line2D, Rect, Triangle2D, Polygon2D, hem, 
+    BoundingBox, Octree) {
     return {
       Vec2 : Vec2,
       Vec3 : Vec3,
@@ -31,13 +36,17 @@ define(
       gen : gen,
       Face3 : Face3,
       Face4 : Face4,
+      FacePolygon : FacePolygon,
       Vec2Array : Vec2Array,
       Vec3Array : Vec3Array,
       Vec4Array : Vec4Array,
       Line2D : Line2D,
       Rect : Rect,
       Triangle2D : Triangle2D,
-      Polygon2D : Polygon2D
+      Polygon2D : Polygon2D,
+      hem : hem,
+      BoundingBox : BoundingBox,
+      Octree : Octree
     };
   }
 );
