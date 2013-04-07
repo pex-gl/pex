62d972f64965aaa9bca9b25396ea16b8ff7faef2
Vorg <marcin.ignac@gmail.com>
2013-04-07 23:25:30 +0100

    Added HEExtrude

---
+++ src/pex/geom/hem/HEExtrude.js
@@ -0,0 +1,58 @@
+define([
+  'pex/geom/hem/HEMesh',
+  'pex/geom/hem/HEVertex',
+  'pex/geom/hem/HEEdge',
+  'pex/geom/hem/HEFace',
+  'pex/geom/Vec3'
+  ],
+  function(HEMesh, HEVertex, HEEdge, HEFace, Vec3) {
+
+  function Extrude() {
+  }
+
+  HEMesh.prototype.extrude = function(height) {
+    var numFaces = this.faces.length;
+
+    var self = this;
+    //this.getSelectedFaces().forEach(function(face, i) {
+    this.faces.forEach(function(face, i) {
+      var normal = face.getNormal();
+      var edge = face.edge;
+      var lastEdge = edge.findPrev();
+      var edgeToSplit = edge;
+      var prevNewEdge = null;
+      var center = face.getCenter();
+      var newEdges = [];
+
+      //we split all the corners within the face effectively adding new internal vertices
+      do {
+        //var newVertexPos = edgeToSplit.vert.added(normal.scaled(height));
+        var newVertexPos = Vec3.clone(normal);
+        Vec3.scale(newVertexPos, newVertexPos, height);
+        Vec3.add(newVertexPos, newVertexPos, edgeToSplit.vert.position);
+
+        edgeToSplit.vert.edge = edgeToSplit; //TODO: fix that, making sure we split the right face
+        var newEdge = self.splitVertex(edgeToSplit.vert, newVertexPos);
+        newEdges.push(newEdge);
+        if (edgeToSplit == lastEdge) {
+          break;
+        }
+        edgeToSplit = edgeToSplit.next;
+      } while(edgeToSplit != edge);
+
+      //go through all new corners and cut out faces from them
+      var prevCornerEdge = newEdges[newEdges.length-1].next;
+      for(var i=0; i<newEdges.length; i++) {
+        //we remember what's the next edge pointing to a new corner as
+        //this might change when we add new face
+        var tmp = newEdges[i].next;
+        var newFace = self.splitFace(newEdges[i].next, prevCornerEdge);
+        prevCornerEdge = tmp;
+      }
+    });
+
+    return this;
+  }
+
+  return Extrude;
+});
