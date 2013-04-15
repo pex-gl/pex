0b8272e61da7cb51016f935f836b002112038fee
Vorg <marcin.ignac@gmail.com>
2013-04-15 14:04:55 +0900

    Updated Diffuse lighting material

--- src/pex/materials/Diffuse.glsl
+++ src/pex/materials/Diffuse.glsl
@@ -2,27 +2,15 @@
 
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelWorldMatrix;
-uniform mat4 normalMatrix;
 uniform float pointSize;
-uniform vec3 lightPos;
 attribute vec3 position;
 attribute vec3 normal;
-varying vec3 ecPosition;
-varying vec3 ecLightPos;
 varying vec3 vNormal;
 
 void main() {
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   gl_PointSize = pointSize;
   vNormal = normal;
-
-  vec3 wPos = (modelWorldMatrix * vec4(position, 1.0)).xyz;
-
-  ecPosition = (modelViewMatrix * vec4(position, 1.0)).xyz;
-  ecLightPos = (viewMatrix * vec4(lightPos, 1.0)).xyz;
-  vNormal = (viewMatrix * normalMatrix * vec4(normal, 1.0)).xyz;
 }
 
 #endif
@@ -31,17 +19,15 @@ void main() {
 
 uniform vec4 ambientColor;
 uniform vec4 diffuseColor;
-varying vec3 ecPosition;
-varying vec3 ecLightPos;
+uniform vec3 lightPos;
+uniform float wrap;
 varying vec3 vNormal;
 
-
 void main() {
+  vec3 L = normalize(lightPos);
   vec3 N = normalize(vNormal);
-  vec3 L = normalize(ecLightPos - ecPosition);
-  float NdotL = max(0.0, dot(N, L));
-  gl_FragColor.rgb = vec3(0.2 + NdotL);//ambientColor + NdotL * diffuseColor;
-  gl_FragColor.a = 1.0;
+  float NdotL = max(0.0, (dot(N, L) + wrap) / (1.0 + wrap));
+  gl_FragColor = ambientColor + NdotL * diffuseColor;
 }
 
 #endif
--- src/pex/materials/Diffuse.js
+++ src/pex/materials/Diffuse.js
@@ -13,10 +13,11 @@ define([
     var program = new Program(DiffuseGLSL);
 
     var defaults = {
+      wrap: 1,
       pointSize : 1,
-      lightPos : Vec3.create(10, 10, 10),
-      ambientColor : Vec4.create(0, 0, 0, 1),
-      diffuseColor : Vec4.create(1, 1, 1, 1)
+      lightPos : Vec3.fromValues(10, 20, 30),
+      ambientColor : Vec4.fromValues(0, 0, 0, 1),
+      diffuseColor : Vec4.fromValues(1, 1, 1, 1)
     };
 
     var uniforms = ObjectUtils.mergeObjects(defaults, uniforms);
