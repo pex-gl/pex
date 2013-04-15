513e63663b01a1dcee750048138ea525cf8f68a7
Vorg <marcin.ignac@gmail.com>
2013-04-15 14:25:23 +0900

    Added shuffled method to ArrayUtils

--- src/pex/utils/ArrayUtils.js
+++ src/pex/utils/ArrayUtils.js
@@ -10,5 +10,19 @@ define([], function() {
     return result;
   }
 
+  ArrayUtils.shuffled = function(list) {
+    var newList = ArrayUtils.range(0, list.length);
+    for(var i=0; i<newList.length; i++) {
+      var j = Math.floor(Math.random() * newList.length);
+      var tmp = newList[i];
+      newList[i] = newList[j];
+      newList[j] = tmp;
+    }
+    for(var i=0; i<newList.length; i++) {
+      newList[i] = list[newList[i]];
+    }
+    return newList;
+  }
+
   return ArrayUtils;
 });
