6d280244fc594bf0f284d939c6e0fb46148cefab
Vorg <marcin.ignac@gmail.com>
2013-05-29 00:10:26 +0100

    Trying simplified commonjs wrapper

--- src/pex.js
+++ src/pex.js
@@ -1,26 +1,24 @@
 //Module wrapper for the whole Pex library.
-define(
-  [
-    'pex/geom',
-    'pex/utils',
-    'pex/sys',
-    'pex/gl',
-    'pex/materials',
-    'pex/scene',
-    'pex/fx',
-    'pex/gui'
-  ],
-  function(geom, utils, sys, gl, materials, scene, fx, gui) {
-    return {
-      geom : geom,
-      utils : utils,
-      sys : sys,
-      gl : gl,
-      materials : materials,
-      scene : scene,
-      fx : fx,
-      require : sys.Require, //shortcut,
-      gui : gui
-    };
-  }
-)
+define(function(require) {
+  var geom = require('pex/geom');
+  var utils = require('pex/utils');
+  var sys = require('pex/sys');
+  var gl = require('pex/gl');
+  var materials = require('pex/materials');
+  var scene = require('pex/scene');
+  var fx = require('pex/fx');
+  var gui = require('pex/gui');
+  var test = require('pex/test/test');
+
+  return {
+    geom : geom,
+    utils : utils,
+    sys : sys,
+    gl : gl,
+    materials : materials,
+    scene : scene,
+    fx : fx,
+    require : sys.Require, //shortcut,
+    gui : gui
+  };
+});
