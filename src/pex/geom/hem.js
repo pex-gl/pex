25f25f6bd251aab0b885f6268decf361a9c7593a
Vorg <marcin.ignac@gmail.com>
2013-05-28 15:35:02 +0100

    Updated hem package

--- src/pex/geom/hem.js
+++ src/pex/geom/hem.js
@@ -2,10 +2,12 @@ define(
   [
     'pex/geom/hem/HEMesh',
     'pex/geom/hem/HESelection',
+    'pex/geom/hem/HEMarking',
     'pex/geom/hem/HEGeometryConverter',
-    'pex/geom/hem/HEExtrude'
+    'pex/geom/hem/HEExtrude',
+    'pex/geom/hem/HECatmullClark',
   ],
-  function(HEMesh, HESelection, HEGeometryConverter, HEExtrude) {
+  function(HEMesh, HESelection, HEMarking, HEGeometryConverter, HEExtrude, HECatmullClark) {
     return function() {
       return new HEMesh();
     }
