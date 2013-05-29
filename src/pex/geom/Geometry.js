121e0b912b8ea55b19e2078d9061e4bcb975d78f
Vorg <marcin.ignac@gmail.com>
2013-05-29 14:34:47 +0100

    Removed dead code from Geometry

--- src/pex/geom/Geometry.js
+++ src/pex/geom/Geometry.js
@@ -29,37 +29,3 @@ define(function(require) {
 
   })();
 });
-
-/*
-
-    for(var attribName in this.attribs) {
-      var attrib = this.attribs[attribName];
-      attrib.isDirty = true;
-      attrib.elementSize = elementSizeMap[attrib.type];
-      if (attrib.type == 'Vec2') { attrib.data = new Vec2Array(attrib.length); }
-      if (attrib.type == 'Vec3') { attrib.data = new Vec3Array(attrib.length); }
-      if (attrib.type == 'Vec4') { attrib.data = new Vec4Array(attrib.length); }
-    }
-  }
-
-  Geometry.prototype.assureSize = function(numVertices) {
-    for(var attribName in this.attribs) {
-      var attrib = this.attribs[attribName];
-      if (attrib.length < numVertices) {
-        var newSize = Math.floor(numVertices * 2);
-        var newAttribData;
-        if (attrib.type == 'Vec2') { newAttribData = new Vec3Array(newSize); }
-        if (attrib.type == 'Vec3') { newAttribData = new Vec3Array(newSize); }
-        if (attrib.type == 'Vec4') { newAttribData = new Vec3Array(newSize); }
-        newAttribData.buf.set(attrib.data.buf);
-        attrib.length = newSize;
-        attrib.data = newAttribData;
-        attrib.isDirty = true;
-      }
-    }
-  }
-
-  return Geometry;
-});
-*/
-
