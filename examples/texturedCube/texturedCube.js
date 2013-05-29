3c4ddf6b4c54e400df9508507c29ea72e380a221
Vorg <marcin.ignac@gmail.com>
2013-05-29 16:11:14 +0100

    Ported TexturedCube example

--- examples/texturedCube/texturedCube.js
+++ examples/texturedCube/texturedCube.js
@@ -12,7 +12,7 @@ pex.sys.Window.create({
   },
   init: function() {
     this.camera = new pex.scene.Camera(60, this.width/this.height);
-    new pex.scene.Arcball(this, this.camera, 2);
+    new pex.scene.Arcball(this, this.camera, 5);
     var cube = new pex.geom.gen.Cube();
 
     var texture = pex.gl.Texture2D.load('assets/plask.png', function(texture) {
@@ -27,9 +27,9 @@ pex.sys.Window.create({
 
     for(var i=0; i<100; i++) {
       var rot = pex.geom.Quat.create();
-      pex.geom.Quat.setAxisAngle(rot, pex.geom.Vec3.fromValues(0, 1, 0), Math.random() * Math.PI);
+      rot.setAxisAngle(pex.geom.Vec3.create(0, 1, 0), Math.random() * Math.PI);
       this.instances.push({
-        position: pex.geom.Vec3.fromValues(
+        position: pex.geom.Vec3.create(
           Math.random()*2 - 1,
           Math.random()*2 - 1,
           Math.random()*2 - 1
@@ -45,16 +45,11 @@ pex.sys.Window.create({
     gl.enable(gl.DEPTH_TEST);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    //pex.geom.Quat.setAxisAngle(this.mesh.rotation, this.camera.up, pex.utils.Time.seconds);
-
     if (this.mesh) this.instances.forEach(function(instance) {
       this.mesh.position = instance.position;
       this.mesh.rotation = instance.rotation;
       this.mesh.draw(this.camera);
     }.bind(this));
-
-    //var mem = process.memoryUsage();
-    //console.log(Math.floor(mem.rss/10000)/100, Math.floor(mem.heapTotal/10000)/100, Math.floor(mem.heapUsed/10000)/100);
   }
 });
 
