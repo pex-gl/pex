8a3b041f59f0f8186b293fe026b62156535d47db
Vorg <marcin.ignac@gmail.com>
2013-05-29 14:10:58 +0100

    Removed Vec*Arrays

--- src/pex/geom.js
+++ src/pex/geom.js
@@ -12,9 +12,6 @@ define(
     'pex/geom/Face3',
     'pex/geom/Face4',
     'pex/geom/FacePolygon',
-    'pex/geom/Vec2Array',
-    'pex/geom/Vec3Array',
-    'pex/geom/Vec4Array',
     'pex/geom/Line2D',
     'pex/geom/Rect',
     'pex/geom/Triangle2D',
@@ -24,7 +21,7 @@ define(
     'pex/geom/Octree'
   ],
   function(Vec2, Vec3, Vec4, Mat3, Mat4, Quat, Geometry, gen,
-    Edge, Face3, Face4, FacePolygon, Vec2Array, Vec3Array, Vec4Array, Line2D, Rect, Triangle2D, Polygon2D, hem, 
+    Edge, Face3, Face4, FacePolygon, Line2D, Rect, Triangle2D, Polygon2D, hem, 
     BoundingBox, Octree) {
     return {
       Vec2 : Vec2,
@@ -39,9 +36,6 @@ define(
       Face3 : Face3,
       Face4 : Face4,
       FacePolygon : FacePolygon,
-      Vec2Array : Vec2Array,
-      Vec3Array : Vec3Array,
-      Vec4Array : Vec4Array,
       Line2D : Line2D,
       Rect : Rect,
       Triangle2D : Triangle2D,
