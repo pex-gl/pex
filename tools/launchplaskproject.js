d0b3e77f88b9ad043227b28ad2b35c1e2a72cbe7
vorg <marcin.ignac@gmail.com>
2014-01-17 15:05:14 +0100

    Added script for searching directory tree for main.js file for project launching.

---
+++ tools/launchplaskproject.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+
+var plaskPath = process.argv[2];
+var scriptPath = process.argv[3];
+var dir = path.dirname(scriptPath);
+
+var iterationCount = 0;
+while(dir != '/' && ++iterationCount < 50) {
+  var mainFile = path.join(dir,'/','main.js');
+  if (fs.existsSync(mainFile)) {
+    console.log(dir);
+    var child = spawn(plaskPath,[mainFile], {cwd:dir+'/'});
+    child.on('exit', function() {
+    })
+    child.stdout.on('data', function(data) {
+      console.log(data.toString());
+    });
+    child.stderr.on('data', function(data) {
+      console.log(data.toString());
+    });
+    break;
+  }
+  else {
+    dir = path.resolve(path.join(dir, '/', '..'));
+  }
+}
