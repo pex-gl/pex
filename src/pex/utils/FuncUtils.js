79709d20ee32dd3bfa00485a853f2d6d8a825691
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-02-11 10:58:34 +0100

    added FuncUtils with functional programming inspired functions

--- src/pex/utils/FuncUtils.js
+++ src/pex/utils/FuncUtils.js
@@ -1,6 +1,13 @@
 define([], function() {
-  function FuncUtils() {
-  }
+  function FuncUtils() {}
+
+  FuncUtils.not = function(x) {
+    return !x;
+  };
+
+  FuncUtils.equal = function(a, b) {
+    return (a === b);
+  };
 
   FuncUtils.autoCurry = function(func) {
     var len = func.length;
@@ -10,7 +17,7 @@ define([], function() {
       args = args.concat(Array.prototype.slice.call(arguments));
       return (args.length >= len) ? func.apply(this, args.splice(0)) : next;
     }
-  }
+  };
 
   FuncUtils.compose = function() {
     var funcs = Array.prototype.slice.apply(arguments);
@@ -22,7 +29,41 @@ define([], function() {
       }
       return args[0];
     };
-  }
+  };
+
+  FuncUtils.repeatedly = function(num, func) {
+    var i = 0;
+    var values = [];
+
+    for (i = 0; i < num; ++i) {
+      values.push(func(i));
+    }
+
+    return values;
+  };
+
+  FuncUtils.repeat = function(num, value) {
+    return FuncUtils.repeatedly(num, function() { return value; });
+  };
+
+  FuncUtils.dispatch = function() {
+    var funcs = Array.prototype.slice.apply(arguments);
+    var len = funcs.length;
+
+    return function(target) {
+      var value = undefined;
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      for (var i = 0; i < len; ++i) {
+        var func = funcs[i];
+        value = func.apply(func, Array.prototype.concat(target, args));
+
+        if (value) return value;
+      }
+
+      return value;
+    };
+  };
 
   return FuncUtils;
 });
