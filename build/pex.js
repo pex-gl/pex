dc9524f770b60ed931fccebbeec77238b29a436e
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-03-26 11:00:02 +0100

    added missing FuncUtils inside pex/utils

--- build/pex.js
+++ build/pex.js
@@ -5022,6 +5022,63 @@ define('pex/utils/ObjectUtils',[], function () {
   };
   return ObjectUtils;
 });
+define('pex/utils/FuncUtils',[], function () {
+  function FuncUtils() {
+  }
+  FuncUtils.not = function (x) {
+    return !x;
+  };
+  FuncUtils.equal = function (a, b) {
+    return a === b;
+  };
+  FuncUtils.autoCurry = function (func) {
+    var len = func.length;
+    var args = [];
+    return function next() {
+      args = args.concat(Array.prototype.slice.call(arguments));
+      return args.length >= len ? func.apply(this, args.splice(0)) : next;
+    };
+  };
+  FuncUtils.compose = function () {
+    var funcs = Array.prototype.slice.apply(arguments);
+    return function () {
+      var args = Array.prototype.slice.apply(arguments);
+      for (var i = funcs.length - 1; i >= 0; --i) {
+        args = [funcs[i].apply(this, args)];
+      }
+      return args[0];
+    };
+  };
+  FuncUtils.repeatedly = function (num, func) {
+    var i = 0;
+    var values = [];
+    for (i = 0; i < num; ++i) {
+      values.push(func(i));
+    }
+    return values;
+  };
+  FuncUtils.repeat = function (num, value) {
+    return FuncUtils.repeatedly(num, function () {
+      return value;
+    });
+  };
+  FuncUtils.dispatch = function () {
+    var funcs = Array.prototype.slice.apply(arguments);
+    var len = funcs.length;
+    return function (target) {
+      var value;
+      var args = Array.prototype.slice.call(arguments, 1);
+      for (var i = 0; i < len; ++i) {
+        var func = funcs[i];
+        value = func.apply(func, Array.prototype.concat(target, args));
+        if (value)
+          return value;
+      }
+      return value;
+    };
+  };
+  return FuncUtils;
+});
 define('pex/sys/Platform',[], function () {
   var isPlask = typeof window === 'undefined' && typeof process === 'object';
   var isBrowser = typeof window === 'object' && typeof document === 'object';
@@ -7424,16 +7481,18 @@ define('pex/utils',[
   'pex/utils/Log',
   'pex/utils/Time',
   'pex/utils/ObjectUtils',
+	'pex/utils/FuncUtils',
   'pex/utils/MathUtils',
   'pex/utils/ArrayUtils',
   'pex/utils/ObjReader',
   'pex/utils/ObjWriter',
   'pex/utils/MovieRecorder'
-], function (Log, Time, ObjectUtils, MathUtils, ArrayUtils, ObjReader, ObjWriter, MovieRecorder) {
+], function (Log, Time, ObjectUtils, FuncUtils, MathUtils, ArrayUtils, ObjReader, ObjWriter, MovieRecorder) {
   return {
     Log: Log,
     Time: Time,
     ObjectUtils: ObjectUtils,
+		FuncUtils: FuncUtils,
     MathUtils: MathUtils,
     ArrayUtils: ArrayUtils,
     ObjReader: ObjReader,
@@ -7441,6 +7500,7 @@ define('pex/utils',[
     MovieRecorder: MovieRecorder
   };
 });
+
 (function() {
   define('pex/gl/Program',['require','pex/gl/Context','pex/sys/IO','pex/utils/Log'],function(require) {
     var Context, IO, Log, Program, kFragmentShaderPrefix, kVertexShaderPrefix;
